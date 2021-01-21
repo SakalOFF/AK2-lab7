e5bc975006fce4c6413c1438bd4cd599fcec9557
Volodymyr Sokolov <Volodymyr_Sokolov@epam.com>
2021-01-21 03:08:09 +0200

    initial commit

---
+++ hello2.c
@@ -0,0 +1,31 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/ktime.h>
+#include <hello1.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("AK-2 lab_7: hello2.c");
+MODULE_AUTHOR("Volodymyr Sokolov");
+
+static uint n = 1;
+
+module_param(n, uint, 0);
+MODULE_PARM_DESC(n, "How many hellos to print\n");
+
+static int __init init_hello(void)
+{
+	pr_info("hello2 init\n");
+	print_hello(n);
+	return 0;
+}
+
+static void __exit exit_hello(void)
+{
+	pr_info("hello2 exit\n");
+}
+
+module_init(init_hello);
+module_exit(exit_hello);
